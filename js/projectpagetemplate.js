936cf3b036076f394b78dbf12b134952ca5366db
Max Dahl <github@daagur.de>
2022-03-25 12:38:20 +0100

    implemented heart button

--- js/projectpagetemplate.js
+++ js/projectpagetemplate.js
@@ -1,112 +1,84 @@
-// List of Pictures for the Grid
-
-let gridImagesSrc = [
-    "img/photos/portraet/marie/Marie_104.jpg",
-    "img/photos/portraet/marie/Marie_077.jpg",
-    "img/photos/portraet/marie/Marie_003.jpg",
-    "img/photos/portraet/marie/Marie_033.jpg",
-    "img/photos/portraet/marie/Marie_011.jpg",
-    
-    "img/photos/portraet/marie/Marie_006.jpg",
-    "img/photos/portraet/marie/Marie_075.jpg",
-    "img/photos/portraet/marie/Marie_072.jpg",
-    "img/photos/portraet/marie/Marie_046.jpg",
-    "img/photos/portraet/marie/Marie_108.jpg",
-    // unused images
-    //"img/photos/portraet/marie/Marie_104_cropped.jpg",
-]
-
-for (let i = 0; i < gridImagesSrc.length; i++) {
-    document.querySelector(`.ppg-pic${i+1} img`).src = gridImagesSrc[i];
+//Heart Btn
+function toggleLike() {
+  if (document.getElementById("favourite").innerHTML === "favorite_border") {
+    document.getElementById("favourite").innerHTML = "favorite";
+  } else {
+    document.getElementById("favourite").innerHTML = "favorite_border";
+  }
 }
 
+// List of Pictures for the Grid
+let gridImagesSrc = [
+  "img/photos/portraet/marie/Marie_104.jpg",
+  "img/photos/portraet/marie/Marie_077.jpg",
+  "img/photos/portraet/marie/Marie_003.jpg",
+  "img/photos/portraet/marie/Marie_033.jpg",
+  "img/photos/portraet/marie/Marie_011.jpg",
 
+  "img/photos/portraet/marie/Marie_006.jpg",
+  "img/photos/portraet/marie/Marie_075.jpg",
+  "img/photos/portraet/marie/Marie_072.jpg",
+  "img/photos/portraet/marie/Marie_046.jpg",
+  "img/photos/portraet/marie/Marie_108.jpg",
+  // unused images
+  //"img/photos/portraet/marie/Marie_104_cropped.jpg",
+];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for (let i = 0; i < gridImagesSrc.length; i++) {
+  document.querySelector(`.ppg-pic${i + 1} img`).src = gridImagesSrc[i];
+}
 
 // TURNING THE OVERLAY ON / OFF
 
 function turnOnOverlay() {
-    document.getElementById("overlayOverProjectViewPage").style.display = "flex";    
+  document.getElementById("overlayOverProjectViewPage").style.display = "flex";
 }
 
 function turnOffOverlay() {
-    document.getElementById("overlayOverProjectViewPage").style.display = "none";
-} 
-
-
-
-
-
-
-
-
-
+  document.getElementById("overlayOverProjectViewPage").style.display = "none";
+}
 
 // APPROACH #2  FOR SLIDER
 
-
-
 // // Array with picture urls
 let pictures = [
-    "img/photos/portraet/marie/Marie_104.jpg",
-    "img/photos/portraet/marie/Marie_077.jpg",
-    "img/photos/portraet/marie/Marie_003.jpg",
-    "img/photos/portraet/marie/Marie_033.jpg",
-    "img/photos/portraet/marie/Marie_011.jpg",
-    
-    "img/photos/portraet/marie/Marie_006.jpg",
-    "img/photos/portraet/marie/Marie_075.jpg",
-    "img/photos/portraet/marie/Marie_072.jpg",
-    "img/photos/portraet/marie/Marie_046.jpg",
-    "img/photos/portraet/marie/Marie_108.jpg",
-    "img/photos/portraet/marie/Marie_104_cropped.jpg",
+  "img/photos/portraet/marie/Marie_104.jpg",
+  "img/photos/portraet/marie/Marie_077.jpg",
+  "img/photos/portraet/marie/Marie_003.jpg",
+  "img/photos/portraet/marie/Marie_033.jpg",
+  "img/photos/portraet/marie/Marie_011.jpg",
+
+  "img/photos/portraet/marie/Marie_006.jpg",
+  "img/photos/portraet/marie/Marie_075.jpg",
+  "img/photos/portraet/marie/Marie_072.jpg",
+  "img/photos/portraet/marie/Marie_046.jpg",
+  "img/photos/portraet/marie/Marie_108.jpg",
+  "img/photos/portraet/marie/Marie_104_cropped.jpg",
 ];
 
 let numberOfCurrentImageForSlider = 0;
 
-console.log(numberOfCurrentImageForSlider)
+console.log(numberOfCurrentImageForSlider);
 
 // Set img.src to url taken from array  (starting value)
-document.getElementById('pictureInSliderGalleryProjectPage').src = pictures[numberOfCurrentImageForSlider];
-
-
-
-
+document.getElementById("pictureInSliderGalleryProjectPage").src =
+  pictures[numberOfCurrentImageForSlider];
 
 // change image number by clicking / sliding
 function slideRight() {
-    if (numberOfCurrentImageForSlider < pictures.length - 1){
-        numberOfCurrentImageForSlider++;    
-        document.getElementById('pictureInSliderGalleryProjectPage').src = pictures[numberOfCurrentImageForSlider];
-    }
+  if (numberOfCurrentImageForSlider < pictures.length - 1) {
+    numberOfCurrentImageForSlider++;
+    document.getElementById("pictureInSliderGalleryProjectPage").src =
+      pictures[numberOfCurrentImageForSlider];
+  }
 }
 function slideLeft() {
-    if (numberOfCurrentImageForSlider > 0){
-        numberOfCurrentImageForSlider--;
-        document.getElementById('pictureInSliderGalleryProjectPage').src = pictures[numberOfCurrentImageForSlider];
-    }
+  if (numberOfCurrentImageForSlider > 0) {
+    numberOfCurrentImageForSlider--;
+    document.getElementById("pictureInSliderGalleryProjectPage").src =
+      pictures[numberOfCurrentImageForSlider];
+  }
 }
-
-
-
-
-
-
-
 
 // APPROACH #1  FOR SLIDER
 
@@ -119,40 +91,33 @@ function slideLeft() {
 // change i with the left - right arrow -buttons
 
 // müsste mit DOM css properties verändern!!        ALTERNATIV: inline style attributes
-// css : 
+// css :
 // i-1th child -> display: none
-// i-th-child -> display: block  
-
-
-
-
-
+// i-th-child -> display: block
 
 // APPROACH #3  FOR SLIDER
 
-// load all picture to Array 
+// load all picture to Array
 
 // change img element to another img element from Array
 
 // BUT: why pre-load large images if user might not even want to view them?
 
-
-
 // BOTTOM IMAGES
 
 let bottomImages = [
-    "img/photos/portraet/marie/Marie_033.jpg",
-    "img/photos/portraet/marie/Marie_075.jpg",
-    "img/photos/portraet/marie/Marie_072.jpg",
-    "img/photos/portraet/marie/Marie_104_cropped.jpg",
+  "img/photos/portraet/marie/Marie_033.jpg",
+  "img/photos/portraet/marie/Marie_075.jpg",
+  "img/photos/portraet/marie/Marie_072.jpg",
+  "img/photos/portraet/marie/Marie_104_cropped.jpg",
 ];
 
 let toAppend;
 
 for (let i = 0; i < bottomImages.length; i++) {
-    toAppend = document.createElement("img");
-    toAppend.src = bottomImages[i];
-    document.querySelector('.project-page-bottom-gallery-container').appendChild(toAppend);
+  toAppend = document.createElement("img");
+  toAppend.src = bottomImages[i];
+  document
+    .querySelector(".project-page-bottom-gallery-container")
+    .appendChild(toAppend);
 }
-
-
